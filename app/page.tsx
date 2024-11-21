bfc61c15d76fb33e1383111560dc4cd28ab72e6c
Pol Sanchez <polsamo2003@gmail.com>
2024-11-21 16:17:56 +0100

    Error corregido

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
-import clsx from "clsx";
 
-export default function Page({ device }: { device: string }) {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className={styles.shape} />

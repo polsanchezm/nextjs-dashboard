fe5db268f2e53bba26730c43b203bc5118b589e6
Pol Sanchez <polsamo2003@gmail.com>
2024-11-22 18:39:54 +0100

    Chapter 16 - Final

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from "@heroicons/react/24/outline";
-import Link from "next/link";
-import { Button } from "@/app/ui/button";
-import { updateInvoice } from "@/app/lib/actions";
+// ...
+import { updateInvoice, State } from "@/app/lib/actions";
+import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,9 +11,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  console.log("invoce ", updateInvoiceWithId);
-  console.log("invoce id ", invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
-  return <form action={updateInvoiceWithId}></form>;
+  return <form action={formAction}></form>;
 }
